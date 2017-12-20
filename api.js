774aaab4fea707a7337ec04423e17b093cd36ac9
Martin Giger <martin@humanoids.be>
2017-12-20 21:27:01 +0100

    feat: add requestPermission API. Not finalized or anything, just there for now.

--- api.js
+++ api.js
@@ -1,4 +1,5 @@
-const {utils: Cu} = Components;
+"use strict";
+const {classes: Cc, interfaces: Ci, results: Cr, utils: Cu} = Components;
 
 Cu.import("resource://gre/modules/XPCOMUtils.jsm");
 
@@ -7,10 +8,20 @@ XPCOMUtils.defineLazyServiceGetter(this, "ParentalControls",
 
 class API extends ExtensionAPI {
   getAPI(context) {
+    const { extension } = context;
+    const buildPermissionURI = (reason) => extension.getURL('/') + encodeURIComponent(reason);
     return {
       parentalControls: {
         async getStatus() {
           return ParentalControls.parentalControlsEnabled;
+        },
+        async requestPermission(reason) {
+          if (!ParentalControls.parentalControlsEnabled) {
+            return true;
+          }
+
+          const permissionURI = buildPermissionURI(reason);
+          return ParentalControls.requestURIOverride(permissionURI, context.contentWindow);
         }
       }
     };
